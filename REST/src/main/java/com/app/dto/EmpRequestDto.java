3a3c3639c76905407545a48c213bd9afd33657b4
NiteeshSati <121075598+NiteeshSati@users.noreply.github.com>
2023-02-07 12:09:43 +0530

    Add files via upload

---
+++ REST/src/main/java/com/app/dto/EmpRequestDto.java
@@ -0,0 +1,25 @@
+package com.app.dto;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class EmpRequestDto {
+	
+	private String dept;
+	private double sal;
+	private LocalDate joindate;
+	
+	
+	
+
+}
